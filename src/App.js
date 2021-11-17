24803bfe8e0f18d5be52fe5110a98ad91b463c92
Dev C. Krishna <dev.c.krishna.123@gmail.com>
2021-11-18 00:38:57 +0530

    Some

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { API } from "./backend"
+
+function App() {
+  return (
+    <div className="App">
+      Hello world
+
+      <h1>{`${API}`}</h1>
+
+    </div>
+  );
+}
+
+export default App;

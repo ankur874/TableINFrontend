77aa6cc7a5dcc862ce8d320b6d43fae2b1355c2d
Dev C. Krishna <dev.c.krishna.123@gmail.com>
2021-11-18 16:41:51 +0530

    redeux added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
-import { API } from "./backend"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import HomeScreen from "./Screens/HomeScreen";
+import SignUpScreen from "./Screens/SignUpScreen";
+import LoginScreen from "./Screens/LoginScreen";
 
 function App() {
   return (
-    <div className="App">
-      Hello world
-
-      <h1>{`${API}`}</h1>
-
-    </div>
+    <Router>
+      <Routes >
+        <Route exact path="/" element={<HomeScreen />} />
+        <Route path='/signup' element={<SignUpScreen />} />
+        <Route path='/login' element={<LoginScreen />} />
+      </Routes>
+    </Router>
   );
 }
 

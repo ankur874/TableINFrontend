df64484f6b8db7ff042966bcb8252ead6394e846
Ankur Attri <ankurattri874@gmail.com>
2021-11-21 16:08:15 +0530

    Ankur Attri

---
+++ src/data/tableData.js
@@ -0,0 +1,32 @@
+export const tableData = [
+  {
+    icon: <i  className="fas hover:scale-150  hover:bg-green-600   fa-border-all"></i>,
+    id: 0,
+    index: 0,
+    key: 0,
+  },
+  {
+    icon: <i className="fas hover:scale-150 hover:bg-green-600  fa-circle"></i>,
+    id: 1,
+    index: 1,
+    key: 1,
+  },
+  {
+    icon: <i className="fas hover:scale-150 hover:bg-green-600  fa-caret-left"></i>,
+    id: 2,
+    index: 2,
+    key: 2,
+  },
+  {
+    icon: <i className="fas hover:scale-150 hover:bg-green-600  fa-dog"></i>,
+    id: 3,
+    index: 3,
+    key: 3,
+  },
+  {
+    icon: <i className="fas hover:scale-150 hover:bg-green-600  fa-cat"></i>,
+    id: 4,
+    index: 4,
+    key: 4,
+  },
+];
